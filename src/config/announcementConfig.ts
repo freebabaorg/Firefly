5a10809a0b144e4f6ea24dbce0b1c970792a761d
freebabaorg <zouqi888999@gmail.com>
2025-10-18 20:59:21 +0800

    Update announcementConfig.ts

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -2,7 +2,7 @@ import type { AnnouncementConfig } from "../types/config";
 
 export const announcementConfig: AnnouncementConfig = {
   title: "公告", // 公告标题
-  content: "欢迎来到我的博客！这是一则示例公告。", // 公告内容
+  content: "FreeBaBa.org 免费巴巴 这是一个翻墙梯子节点免费分享博客，而且每天更新，永久免费！", // 公告内容
   closable: true, // 允许用户关闭公告
   link: {
     enable: true, // 启用链接
