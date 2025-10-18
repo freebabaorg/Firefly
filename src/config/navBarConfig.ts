b19a65f1262202b7b0fdc799fd7ff3d4ba389f5b
freebabaorg <zouqi888999@gmail.com>
2025-10-18 22:39:47 +0800

    Update navBarConfig.ts

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -14,6 +14,12 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 //    links.push(LinkPreset.Anime); //
 //  } //
 
+links.push({
+  name: "技术分享",  // 菜单文本（支持 i18n，如果配置了多语言）
+  url: "/tech-sharing/",  // 链接路径（需确保对应页面存在）
+  icon: "material-symbols:mail",  // 图标
+});
+
   // 支持自定义导航栏链接,并且支持多级菜单
   links.push({
     name: "链接",
